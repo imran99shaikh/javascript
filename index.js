6755b591c548101e6bd269cfa9fa18547038b130
imran shaikh <imranshaikhdws64@gmail.com>
2023-05-31 11:54:24 +0530

    MyFirstRepo

---
+++ index.js
@@ -0,0 +1,17 @@
+const Home=function(){
+    document.getElementById("home");
+    home.innerHTML=`<h1>Home Page</h1>`
+    
+}
+
+const About=function(){
+    document.getElementById("home");
+    home.innerHTML=`<h1>About Page</h1>`
+    
+}
+
+const Service=function(){
+    document.getElementById("home");
+    home.innerHTML=`<h1>Service Page</h1>`
+    
+}
